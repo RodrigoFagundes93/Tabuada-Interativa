db44aa290d2a2def6b9bc6564c69fc9369c8ef3c
Rodrigo Fagundes <rodrigo.aspiradev@gmail.com>
2026-02-26 12:26:09 -0300

    New repository

---
+++ script.js
@@ -0,0 +1,12 @@
+const tela = document.getElementById('tela');
+const titulo = document.getElementById('titulo')
+//inputs
+const numeroInput = document.getElementById('numero');
+const radios = document.getElementById('radios'); 
+
+//botoes
+const btnLimpar = document.getElementById('btnLimpar');
+const btnCalcular = document.getElementById('btnCalc');
+
+
+//Criar funcao para gerar tabuada - parametro numero e operador
