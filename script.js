55a1b0a3b677f067e56fe3db7162f7e8d65c1490
Rodrigo Fagundes <rodrigo.aspiradev@gmail.com>
2026-02-26 14:04:29 -0300

    Atualiza index, script e estilo da tabuada

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const tela = document.getElementById('tela');
 const titulo = document.getElementById('titulo')
 //inputs
 const numeroInput = document.getElementById('numero');
-const radios = document.getElementById('radios'); 
+const radios = document.querySelectorAll('#radios label input');
 
 //botoes
 const btnLimpar = document.getElementById('btnLimpar');
@@ -10,3 +10,57 @@ const btnCalcular = document.getElementById('btnCalc');
 
 
 //Criar funcao para gerar tabuada - parametro numero e operador
+function gerarTabuada(){
+    
+    tela.textContent = "";
+    const numero = parseInt(numeroInput.value)
+
+    if(isNaN(numero)) {
+        alert('Digite um número válido!');
+        return;
+    }
+
+    // pegar operador selecionado
+    let operador;
+    for (const radio of radios) {
+        if (radio.checked) {
+            operador = radio.value;
+            break;
+        }
+    }
+
+    if(!operador) {
+        alert('Selecione um operador!');
+        return;
+    }
+        let result;
+    // gerar tabuada
+    for (let i = 1; i <= 10; i++) {
+    
+        if (operador === '+') {
+            result = numero + i;
+        } if (operador === '-') {
+            result = numero - i;
+        } if (operador === '*') {
+            result = numero * i;
+        } if (operador === '/') {
+            result = (numero / i).toFixed(2);
+        }
+        titulo.textContent = `${numero}`
+        const li = document.createElement('li');
+        li.textContent = `${numero} ${operador} ${i} = ${result}`;
+        tela.appendChild(li);
+        numeroInput.value = ""; 
+        
+    }
+
+}
+
+function limparTela(){
+    tela.textContent = "";
+    titulo.textContent = "";
+     numeroInput.value = ""; 
+}
+
+btnCalcular.addEventListener('click',gerarTabuada);
+btnLimpar.addEventListener('click', limparTela)
